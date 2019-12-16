7cebd38e0b67ac2f8d227fd20de43742de123a46
Yahub <seitasanov.yahub@mail.ru>
2019-12-16 19:11:43 +0300

    Updates

---
+++ precache-manifest.b46608cf8e1dfa0af2649e00d62ede81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cee0aac7db34e7dafe5ce624a3bdf47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97a6a3b598df08fc433a",
+    "url": "./static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "00e62eac2a3701f298a3",
+    "url": "./static/css/main.b22b80c8.chunk.css"
+  },
+  {
+    "revision": "97a6a3b598df08fc433a",
+    "url": "./static/js/2.bdf150ab.chunk.js"
+  },
+  {
+    "revision": "00e62eac2a3701f298a3",
+    "url": "./static/js/main.0dfbb32f.chunk.js"
+  },
+  {
+    "revision": "0c8f187e103a23b100ee",
+    "url": "./static/js/runtime-main.a556eca6.js"
+  }
+]);
