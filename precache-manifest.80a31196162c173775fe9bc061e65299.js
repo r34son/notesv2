6d952ad74bcbff13eacf63bb6eedb2a7b3844746
Yahub <seitasanov.yahub@mail.ru>
2019-12-16 19:13:58 +0300

    Updates

---
+++ precache-manifest.80a31196162c173775fe9bc061e65299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa337a427ddac1b3638dcc088ad931a",
+    "url": "/notesv2/index.html"
+  },
+  {
+    "revision": "3c88367f202f02d8bb4a",
+    "url": "/notesv2/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "00e62eac2a3701f298a3",
+    "url": "/notesv2/static/css/main.b22b80c8.chunk.css"
+  },
+  {
+    "revision": "3c88367f202f02d8bb4a",
+    "url": "/notesv2/static/js/2.17c77ee2.chunk.js"
+  },
+  {
+    "revision": "00e62eac2a3701f298a3",
+    "url": "/notesv2/static/js/main.0dfbb32f.chunk.js"
+  },
+  {
+    "revision": "1c2713a31dc70d1e1f66",
+    "url": "/notesv2/static/js/runtime-main.9acd2801.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b46608cf8e1dfa0af2649e00d62ede81.js"
+  "/notesv2/precache-manifest.80a31196162c173775fe9bc061e65299.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/notesv2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
