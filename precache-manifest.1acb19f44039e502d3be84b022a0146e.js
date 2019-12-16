9ac2b7665013155192645fdd20cda2ece0ad993e
Yahub <seitasanov.yahub@mail.ru>
2019-12-16 21:28:36 +0300

    Updates

---
+++ precache-manifest.1acb19f44039e502d3be84b022a0146e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c3019859bcea171718f2fdb3379257",
+    "url": "/notesv2/index.html"
+  },
+  {
+    "revision": "73b7c244d862ed888530",
+    "url": "/notesv2/static/css/2.bc3aac6f.chunk.css"
+  },
+  {
+    "revision": "0d1104c91687b3105d42",
+    "url": "/notesv2/static/css/main.b22b80c8.chunk.css"
+  },
+  {
+    "revision": "73b7c244d862ed888530",
+    "url": "/notesv2/static/js/2.78e44197.chunk.js"
+  },
+  {
+    "revision": "0d1104c91687b3105d42",
+    "url": "/notesv2/static/js/main.0019ec61.chunk.js"
+  },
+  {
+    "revision": "1c2713a31dc70d1e1f66",
+    "url": "/notesv2/static/js/runtime-main.9acd2801.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/notesv2/precache-manifest.654f1453263e874643848c29b78aee7c.js"
+  "/notesv2/precache-manifest.1acb19f44039e502d3be84b022a0146e.js"
 );
 
 self.addEventListener('message', (event) => {
